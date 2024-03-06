ba14a48a23e0640bc108590f83da292d11783470
AKASH021001-0202 <nakashn007@gmail.com>
2024-03-06 12:29:01 +0530

    3rd commit

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ const getDetailsBtn = document.querySelector('#get-details-btn');
 const detailsContainer = document.querySelector('#details-container');
 const searchPincode = document.querySelector('#search-pincode');
 
-getDetailsBtn.addEventListener('click', () => {
-  const pincode = pincodeInput.value;
-  if (pincode !== '') {
-    fetch(`https://api.postalpincode.in/pincode/${pincode}`)
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
       .then(response => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          reject(new Error('Network response was not ok'));
         }
         return response.json();
       })
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+getDetailsBtn.addEventListener('click', () => {
+  const pincode = pincodeInput.value;
+  if (pincode !== '') {
+    fetchData(`https://api.postalpincode.in/pincode/${pincode}`)
       .then(data => {
         displayDetails(data);
         pincodeInput.value = '';
@@ -25,22 +36,19 @@ getDetailsBtn.addEventListener('click', () => {
   }
 });
 
+
 function displayDetails(data) {
-  console.log(data)
   searchPincode.innerHTML ='';
   searchPincode.textContent = `POST OFFICES LOCATED WITHIN THE PIN CODE : ${pincodeInput.value}`;
   
   detailsContainer.innerHTML = '';
 
   data[0].PostOffice.forEach(postOffice => {
-    
     const card = document.createElement('div');
     card.classList.add('card');
     card.innerHTML = `
       <h2>${postOffice.Name}</h2>
-     
       <p>PIN Code: ${postOffice.Pincode}</p>
-
       <p>Circle: ${postOffice.Circle}</p>
       <p>District: ${postOffice.District}</p>
       <p>Division: ${postOffice.Division}</p>
@@ -51,7 +59,6 @@ function displayDetails(data) {
     detailsContainer.appendChild(card);
   });
 }
-
 
 function displayError(message) {
   detailsContainer.innerHTML = `<p class="error">${message}</p>`;
